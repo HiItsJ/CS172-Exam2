b10cb2b870a97deb3fb6c692766729538da80bd0
Josiah Gallegos <jgallegos20@my.whitworth.edu>
2016-11-10 17:33:18 -0800

    Exam2 completion

--- Exam2/Exam2/City.cpp
+++ Exam2/Exam2/City.cpp
@@ -12,17 +12,22 @@
 #include "City.hpp"
 
 City::City(string cityName){
+    string fileName = cityName + ".txtx";
     theCityName = cityName;
-    string fileName = cityName + ".txt";
-    ofstream fout(fileName, ios::in);
-    fout.open(fileName);
-    for(int x = 0; x<citizenList.size(); x++){
-        fout << citizenList[x];
+    ifstream fin(fileName, ios::out);
+    fin.open(fileName);
+    for (int x = 0; !fin.eof(); x++){
+        fin>>citizenList[x];
     }
-    fout.close(fileName);
 }
 City::~City(){
-    
+    string fileName = theCityName + ".txt";
+    ofstream fout(fileName, ios::in);
+    fout.open(fileName);
+    for (int x = 0; x<citizenList.size(); x++){
+        fout << citizenList[x];
+    }
+    fout.close();
 }
 string City::getCityName(){
     return theCityName;
@@ -37,9 +42,19 @@ void City::addCitizen(Citizen *citizen){
     citizenList.push_back(citizen);
 }
 Citizen* City::getCitizenWithId(int id){
-    
+    for (int x = 0; x<citizenList.size(); x++){
+        if (citizenList[x]->getId() == id){
+            return citizenList[x];
+        }
+    }
+    return citizenList[0];
 }
 vector<Citizen*> City::getCitizensForFavoriteColor(string color){
     vector<Citizen*> citizenColor;
-    
+    for (int x = 0; x<citizenList.size(); x++){
+        if (citizenList[x]->getFavoriteColor() == color){
+            citizenColor.push_back(citizenList[x]);
+        }
+    }
+    return citizenColor;
 }
--- Exam2/Exam2/main.cpp
+++ Exam2/Exam2/main.cpp
@@ -9,8 +9,8 @@
 // I affirm that all code given below was written solely by me, Josiah Gallegos, and that any help I received adhered to the rules stated for this exam.
 
 #include <iostream>
-#include "Citizen.h"
-#include "City.h"
+#include "Citizen.hpp"
+#include "City.hpp"
 using namespace std;
 int main() {
     
